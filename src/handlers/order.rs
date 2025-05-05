1aa3e71f81f48fc36301c6501161954b0adb02e7
jindra12 <jindra12.underdark@gmail.com>
2025-05-05 19:22:56 +0200

    Remove log

--- src/handlers/order.rs
+++ src/handlers/order.rs
@@ -58,7 +58,6 @@ pub async fn process_order(
             .await
             .map_err(|_| OrderError::InternalError)
     } else {
-        tracing::info!("No payload found");
         return state
             .order_status(&order_id)
             .await
